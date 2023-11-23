026462f317f4653aa6e9d06263f7907fa8b8c17b
sabinasmith22 <78496806+sabinasmith22@users.noreply.github.com>
2023-11-23 07:32:07 +0000

    Create todo list and auth

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import './style.css';
+
+
+import styles from './style.css';
 import { Amplify } from 'aws-amplify';
 
-import { withAuthenticator } from '@aws-amplify/ui-react'
-import "@aws-amplify/ui-react/styles.css"
+import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import PropTypes from 'prop-types'
 
 import { generateClient } from 'aws-amplify/api';
-import { createToDo } from './graphql/mutations';
-import { listToDos } from './graphql/queries';
-import { onCreateToDo } from './graphql/subscriptions';
+import { createTodo } from './graphql/mutations.ts';
+import { listTodos } from './graphql/queries.ts';
+import { onCreateTodo } from './graphql/subscriptions.ts';
 
-import amplifyconfig from './amplifyconfiguration.json';
-Amplify.configure(amplifyconfig);
+// import amplifyconfig from './graphql/schema.json';
+Amplify.configure();
 
 const client = generateClient();
 
@@ -19,6 +23,25 @@ const MutationResult = document.getElementById('MutationResult');
 const QueryResult = document.getElementById('QueryResult');
 const SubscriptionResult = document.getElementById('SubscriptionResult');
 
+const App = ({ signOut, user }) => {
+  return (
+    <div style={styles.container}>
+      <Heading level={1}>Hello {user.username}</Heading>
+      <Button onClick={signOut}>Sign out</Button>
+      <h2>Amplify Todos</h2>
+      ...
+    </div>
+  );
+}
+
+//
+App.propTypes = {
+  signOut: PropTypes.func,
+  user: PropTypes.object
+}
+
+export default withAuthenticator(App);
+
 async function addTodo() {
   const todo = {
     name: 'Use AppSync',
@@ -26,14 +49,14 @@ async function addTodo() {
   };
 
   return await client.graphql({
-    query: createToDo,
+    query: createTodo,
     variables: { input: todo },
   });
 }
 
 async function fetchTodos() {
     try { 
-      const response = await client.graphql({ query: listToDos });
+      const response = await client.graphql({ query: listTodos });
     
       response.data.listTodos.items.map((todo,i) => {
         QueryResult.innerHTML += `<p>${todo.name} - ${todo.description}</p>`;
@@ -50,7 +73,7 @@ MutationButton.addEventListener('click', (evt) => {
 });
 
 function subscribeToNewTodos() {
-  client.graphql({ query: onCreateToDo }).subscribe({
+  client.graphql({ query: onCreateTodo }).subscribe({
     next: (evt) => {
       const todo = evt.value.data.onCreateTodo;
       SubscriptionResult.innerHTML += `<p>${todo.name} - ${todo.description}</p>`;
@@ -59,16 +82,6 @@ function subscribeToNewTodos() {
   });
 }
 
+
 subscribeToNewTodos();
 fetchTodos();
-
-// function App({ signOut, user}) {
-//   return (
-//     <>
-//       <h1>Hello {user.username}</h1>
-//       <button onCLick={signOut}>Sign out</button>
-//     </>
-//   );
-// }
-
-// export default withAuthenticator(App);
